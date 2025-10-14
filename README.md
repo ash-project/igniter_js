3f86176557e4962682356069aa36b4c522f3ac0b
James Harton <james@harton.nz>
2025-10-14 15:58:45 +1300

    chore: Fix REUSE license attributions

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 <!--
 SPDX-FileCopyrightText: 2024 Shahryar Tavakkoli
+SPDX-FileCopyrightText: 2024 igniter_js contributors <https://github.com/ash-project/igniter_js/graphs.contributors>
 
 SPDX-License-Identifier: MIT
 -->
--- native/igniter_js/src/atoms.rs
+++ native/igniter_js/src/atoms.rs
@@ -1,4 +1,5 @@
 // SPDX-FileCopyrightText: 2024 Shahryar Tavakkoli
+// SPDX-FileCopyrightText: 2024 igniter_js contributors <https://github.com/ash-project/igniter_js/graphs.contributors>
 //
 // SPDX-License-Identifier: MIT
 
